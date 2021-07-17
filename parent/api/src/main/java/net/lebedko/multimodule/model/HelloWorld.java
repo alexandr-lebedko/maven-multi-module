d900b3e45abe279b06b2a83f6ee04dc294e8cb9c
olebedko <alexandr.lebedko@atlas-iac.com>
2021-07-17 21:46:32 +0300

    Multi-module Maven template

---
+++ parent/api/src/main/java/net/lebedko/multimodule/model/HelloWorld.java
@@ -0,0 +1,7 @@
+package net.lebedko.multimodule.model;
+
+public class HelloWorld {
+    public String toString() {
+        return "[api module class] HelloWorld";
+    }
+}
---
+++ parent/core/src/main/java/net/lebedko/multimodule/Main.java
@@ -0,0 +1,10 @@
+package net.lebedko.multimodule;
+
+
+import net.lebedko.multimodule.model.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld());
+    }
+}
